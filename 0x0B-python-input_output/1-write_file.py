f76844d5100073ebe050983ae2e2ef18b21a226e
INNOCENTMUSAKHI <innocentmusakhi@gmail.com>
2023-04-13 09:51:26 -0700

    1x

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""
-Module a function tht writes astring to a tsxt file
+""" Module that contains a function that writes to a text file
 """
 
 
 def write_file(filename="", text=""):
-    """write a string to a text file (UTF8) and returns the number of characters"""
+    """ Function that writes to a text file
 
-    with open(filename, mode='w', encoding='utf-8') as f:
+    Args:
+        filename: filename
+        text: text to write
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
