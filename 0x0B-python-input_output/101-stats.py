25acb8883da67031c020cc16fc610eb925d34b07
INNOCENTMUSAKHI <innocentmusakhi@gmail.com>
2023-04-13 10:51:55 -0700

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 import sys
 
 
-def print_info(file_size, status_codes):
+def print_info():
     print('File size: {:d}'.format(file_size))
 
     for scode, code_times in sorted(status_codes.items()):
@@ -11,48 +10,43 @@ def print_info(file_size, status_codes):
             print('{}: {:d}'.format(scode, code_times))
 
 
-def main():
-    status_codes = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0
-    }
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
 
-    lc = 0
-    file_size = 0
+lc = 0
+file_size = 0
 
-    try:
-        for line in sys.stdin:
-            if lc != 0 and lc % 10 == 0:
-                print_info(file_size, status_codes)
+try:
+    for line in sys.stdin:
+        if lc != 0 and lc % 10 == 0:
+            print_info()
 
-            pieces = line.split()
+        pieces = line.split()
 
-            try:
-                status = int(pieces[-2])
+        try:
+            status = int(pieces[-2])
 
-                if str(status) in status_codes.keys():
-                    status_codes[str(status)] += 1
-            except:
-                pass
+            if str(status) in status_codes.keys():
+                status_codes[str(status)] += 1
+        except ValueError:
+            pass
 
-            try:
-                file_size += int(pieces[-1])
-            except:
-                pass
+        try:
+            file_size += int(pieces[-1])
+        except ValueError:
+            pass
 
-            lc += 1
+        lc += 1
 
-        print_info(file_size, status_codes)
-    except KeyboardInterrupt:
-        print_info(file_size, status_codes)
-        raise
-
-
-if __name__ == '__main__':
-    main()
+    print_info()
+except KeyboardInterrupt:
+    print_info()
+    raise
