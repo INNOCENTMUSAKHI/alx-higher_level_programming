7111ff2a2d1dca4fc749ee71f1a41e2b0c42fcac
INNOCENTMUSAKHI <innocentmusakhi@gmail.com>
2023-04-13 10:43:05 -0700

    1

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,52 +1,46 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 import sys
 
+def print_info(file_size, status_codes):
+    print(f"File size: {file_size}")
 
-def print_info():
-    print('File size: {:d}'.format(file_size))
+    for code, count in sorted(status_codes.items()):
+        if count > 0:
+            print(f"{code}: {count}")
 
-    for scode, code_times in sorted(status_codes.items()):
-        if code_times > 0:
-            print('{}: {:d}'.format(scode, code_times))
+def main():
+    status_codes = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+    }
 
+    line_count = 0
+    file_size = 0
 
-status_codes = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
-
-lc = 0
-file_size = 0
-
-try:
     for line in sys.stdin:
-        if lc != 0 and lc % 10 == 0:
-            print_info()
-
-        pieces = line.split()
-
         try:
-            status = int(pieces[-2])
-
-            if str(status) in status_codes.keys():
-                status_codes[str(status)] += 1
-        except:
+            _, _, _, status_code, _, size, *_ = line.split()
+            status_codes[status_code] += 1
+            file_size += int(size)
+        except (ValueError, KeyError):
             pass
 
-        try:
-            file_size += int(pieces[-1])
-        except:
-            pass
+        line_count += 1
 
-        lc += 1
+        if line_count % 10 == 0:
+            print_info(file_size, status_codes)
 
-    print_info()
-except KeyboardInterrupt:
-    print_info()
-    raise
+    print_info(file_size, status_codes)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
