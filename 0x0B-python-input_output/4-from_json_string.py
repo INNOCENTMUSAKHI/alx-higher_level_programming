2cca20f28a309c425c10f41c987393898360c6c1
INNOCENTMUSAKHI <innocentmusakhi@gmail.com>
2023-04-13 10:01:37 -0700

    4x

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+""" Module that contains a function that returns an object by
+a JSON representation
 """
-A function that returns an object reps by a Json
-"""
-
-
 import json
 
+
 def from_json_string(my_str):
-    """from json string to object"""
-    return json.loads.(my_str)
+    """ Function that returns an object by a JSON representation
+
+    Args:
+        my_str: JSON representation
+
+    Raises:
+        Exception: when the string can't be decoded
+
+    """
+    return json.loads(my_str)
