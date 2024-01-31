35a1faa34f6fc350f09c8ecafd454f49fe786dee
INNOCENTMUSAKHI <innocentmusakhi@gmail.com>
2024-01-31 15:22:52 -0800

    101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,32 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 const id = process.argv[2];
+if (!id) {
+    console.error('Usage: ./script.js <film_id>');
+    process.exit(1);
+}
+
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
+    if (error) {
+        console.error('Error:', error.message);
+    } else {
+        const content = JSON.parse(body);
+        const characters = content.characters;
+
+        for (const characterUrl of characters) {
+            request.get(characterUrl, (error, response, body) => {
+                if (error) {
+                    console.error('Error:', error.message);
+                } else {
+                    const characterData = JSON.parse(body);
+                    console.log(characterData.name);
+                }
+            });
         }
-      });
     }
-  }
 });
+
